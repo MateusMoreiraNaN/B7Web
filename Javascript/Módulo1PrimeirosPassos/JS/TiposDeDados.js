f975796cc9c720e410846ca4c87f0fe15ac3ff34
MateusMoreiraNaN <mateusgodoi117@gmail.com>
2022-12-28 16:58:19 -0300

    modi1 tiposDeDados

--- Javascript/Módulo1PrimeirosPassos/JS/TiposDeDados.js
+++ Javascript/Módulo1PrimeirosPassos/JS/TiposDeDados.js
@@ -1,10 +1,10 @@
-let nome2 = "Bonieky"
-let idade2 = 90
-let peso = 45.7 // .
-let logado = true
-let deslogado = false
-let lista = ["ovo", "massa", "corante"]
-let nomeCompleto = {nomeM: "Mateus", idadeM: 90}
+let nome2 = "Bonieky" // string
+let idade2 = 90 // number
+let peso = 45.7 // . // number
+let logado = true // boolean
+let deslogado = false // boolean
+let lista = ["ovo", "massa", "corante"] // array
+let nomeCompleto = {nomeM: "Mateus", idadeM: 90} // object
 
 console.log(typeof lista);
 console.log(nome2)
@@ -14,3 +14,16 @@ console.log(logado)
 console.log(deslogado)
 console.log(lista)
 console.log(nomeCompleto)
+
+// este é um comentário
+
+/*
+console.log(typeof lista);
+console.log(nome2)
+console.log(idade2)
+console.log(peso)
+console.log(logado)
+console.log(deslogado)
+console.log(lista)
+console.log(nomeCompleto)
+*/
