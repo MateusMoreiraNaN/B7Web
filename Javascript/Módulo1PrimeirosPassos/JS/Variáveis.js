1263efc6fc9b23e8e22a32c666fb34327c5503db
MateusMoreiraNaN <mateusgodoi117@gmail.com>
2022-12-27 23:18:47 -0300

    subir o b7web

---
+++ Javascript/Módulo1PrimeirosPassos/JS/Variáveis.js
@@ -0,0 +1,14 @@
+let x = 10;
+let y = 5;
+
+let soma = x + y;
+
+console.log(soma);
+
+let idade = 90;
+let nome = "Bonieky";
+let estado = "São paulo";
+let cidade = "Arujá";
+
+console.log(`meu nome é ${nome} e tenho ${idade} anos`);
+console.log(`${estado} | ${cidade}`);
