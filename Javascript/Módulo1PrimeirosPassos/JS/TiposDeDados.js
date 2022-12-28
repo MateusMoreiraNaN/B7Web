f3daa17e20108616fd6d007e4c38c5e64a00e325
MateusMoreiraNaN <mateusgodoi117@gmail.com>
2022-12-27 23:52:23 -0300

    tipos de dados

---
+++ Javascript/Módulo1PrimeirosPassos/JS/TiposDeDados.js
@@ -0,0 +1,16 @@
+let nome2 = "Bonieky"
+let idade2 = 90
+let peso = 45.7 // .
+let logado = true
+let deslogado = false
+let lista = ["ovo", "massa", "corante"]
+let nomeCompleto = {nomeM: "Mateus", idadeM: 90}
+
+console.log(typeof lista);
+console.log(nome2)
+console.log(idade2)
+console.log(peso)
+console.log(logado)
+console.log(deslogado)
+console.log(lista)
+console.log(nomeCompleto)
