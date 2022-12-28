87aa6238811ddbdd1ce5009be912600584fb69cb
MateusMoreiraNaN <mateusgodoi117@gmail.com>
2022-12-28 17:09:08 -0300

    EX#1

---
+++ Javascript/Módulo1PrimeirosPassos/JS/ExercícioDeVariáveis.js
@@ -0,0 +1,17 @@
+let carro = "Ferrari"
+
+alert(`Carro: ${carro}`)
+
+console.log(`Carro: ${carro}`)
+
+let bolo = 19.99
+
+alert(`Bolo: ${bolo}`)
+
+console.log(`Bolo: ${bolo}`)
+
+let cidade = "São paulo"
+
+alert(`Cidade: ${cidade}`)
+
+console.log(`Cidade: ${cidade}`);
