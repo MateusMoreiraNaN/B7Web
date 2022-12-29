d457519a1299c981c333a456b2f4d804d7d29fc4
MateusMoreiraNaN <mateusgodoi117@gmail.com>
2022-12-29 19:25:38 -0300

    subir

---
+++ Javascript/Módulo1PrimeirosPassos/JS/Condicional==E===.js
@@ -0,0 +1,15 @@
+let idade1 = "20"
+
+if(idade1 === 20){
+    console.log("Você tem 20 anos");
+}else {
+    console.log("Error");
+}
+
+let idade2 = "20"
+
+if(idade2 == 20){
+    idade2 = idade2 + 15
+    console.log("Idade adicionda ccom sucesso!");
+    console.log(idade2);
+}
---
+++ Javascript/Módulo1PrimeirosPassos/JS/CondicionalIfElse.js
@@ -0,0 +1,14 @@
+let idadeIf = 11;
+
+if(idadeIf > 18){
+    console.log("Maior de idade");
+}else{
+    console.log("Menor de idade");
+}
+
+// <
+// >
+// ==
+// >= 
+// <=
+// !=
