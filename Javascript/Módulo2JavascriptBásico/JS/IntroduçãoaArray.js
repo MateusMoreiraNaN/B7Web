9321dd3c8123c83d9e497cfcc20610f2f5c8a661
MateusMoreiraNaN <mateusgodoi117@gmail.com>
2023-01-05 15:40:15 -0300

    array

---
+++ Javascript/Módulo2JavascriptBásico/JS/IntroduçãoaArray.js
@@ -0,0 +1,16 @@
+//let color = 'blue'
+
+let colors = ['blue', 'red', 'green', 'black']
+
+console.log(colors[0]);
+console.log(colors[2]);
+
+let idades = [12, 25, 90, 3, 54, 80]
+
+console.log(idades[0]);
+
+let lista = ['blabla', idades]
+
+let lista2 = ['blabla', ['xx', 'yy']]
+
+console.log(lista2[1][1]);
