a7ed5d4edfa7b062e1e6011d143e9fdf41952be2
MateusMoreiraNaN <mateusgodoi117@gmail.com>
2023-01-06 18:30:04 -0300

    objetos

---
+++ Javascript/Módulo2JavascriptBásico/JS/Objetos.js
@@ -0,0 +1,21 @@
+let nome = 'Bonieky'
+let nomes = ['Bonieky', 'Pedro'] // Array
+
+let personagem = {//objetos
+    nome: 'Bonieky',
+    idade: 90,
+    olhos: ['preto', 'azul'],
+    pais: 'Brasil',
+    caracteristicas: {
+        stamina: 30,
+        forca: 20
+    },
+    
+}
+
+console.log(personagem);
+console.log(personagem.nome);
+console.log(personagem.caracteristicas.stamina);
+
+console.log(personagem.olhos[1]);
+
