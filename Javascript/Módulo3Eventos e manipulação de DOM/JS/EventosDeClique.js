5a3082b757db17b41e16881bbd4ee13fdcdc7f5e
MateusMoreiraNaN <mateusgodoi117@gmail.com>
2023-01-10 16:01:21 -0300

    dom

---
+++ Javascript/Módulo3Eventos e manipulação de DOM/JS/EventosDeClique.js
@@ -0,0 +1,18 @@
+function clicou(){
+    let n1 = 5
+    let n2 = 5
+   
+    console.log(n1 + n2);
+
+    console.log('Clicou');
+}
+
+let botao = document.querySelector(".botao")
+
+console.log(botao);
+
+botao.addEventListener("click", () => {
+    clicou()
+});
+
+
---
+++ Javascript/Módulo3Eventos e manipulação de DOM/JS/SelecionandoElementos1.js
@@ -0,0 +1,13 @@
+document.getElementById("teste")
+
+document.getElementsByClassName("botao")
+
+document.querySelector("#teste")
+
+document.querySelector(".botao")
+
+document.querySelector("h1")
+
+document.querySelectorAll("li")
+
+document.querySelectorAll("#teste ul li")
