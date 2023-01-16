e5af85caf36ddba8b16acb5b57444928fb351a6b
MateusMoreiraNaN <mateusgodoi117@gmail.com>
2023-01-16 11:33:57 -0300

    color

--- Javascript/Módulo11Projeto: Compra de Pizzas/script.js
+++ Javascript/Módulo11Projeto: Compra de Pizzas/script.js
@@ -1,5 +1,13 @@
-pizzaJson.map((item, index) =>{
-    let pizzaItem = document.querySelector('.models .pizza-item').cloneNode(true)
+const c = (el)=>document.querySelector(el)
+const cs = (el)=>document.querySelectorAll(el)
 
-    document.querySelector('.pizza-area').append(pizzaItem)
+
+pizzaJson.map((item, index) =>{
+    let pizzaItem = c('.models .pizza-item').cloneNode(true)
+
+    pizzaItem.querySelector('.pizza-item--name').innerHTML = item.name
+    pizzaItem.querySelector('.pizza-item--desc').innerHTML = item.description
+    pizzaItem.querySelector('.pizza-item--price').innerHTML = `R$ ${item.price.toFixed(2)}`
+
+    c('.pizza-area').append(pizzaItem)
 })
