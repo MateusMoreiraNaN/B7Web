1ac83568c7cd88ae3c6b857db36d16bd7f3d0e39
MateusMoreiraNaN <mateusgodoi117@gmail.com>
2023-01-25 20:09:34 -0300

    instancia

---
+++ Javascript/Módulo4ObjetoseOrientaçãoaObjetos/Instância.js
@@ -0,0 +1,10 @@
+class Person{
+    constructor(name, age){
+        this.name = name
+        this.age = age
+    }
+}
+
+let p1 = new Person("João", 20)
+let p2 = new Person("Maria", 30)
+let p3 = new Person("Mateus", 40)
