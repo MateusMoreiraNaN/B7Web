c2b41dab15d05469a062322cc6386a0cb84819a7
MateusMoreiraNaN <mateusgodoi117@gmail.com>
2023-01-26 13:23:52 -0300

    age

--- Javascript/Módulo4ObjetoseOrientaçãoaObjetos/Instância.js
+++ Javascript/Módulo4ObjetoseOrientaçãoaObjetos/Instância.js
@@ -1,10 +1,21 @@
 class Person{
+
+    age = 0
+
     constructor(name, age){
         this.name = name
-        this.age = age
+        //this.age = age
     }
 }
 
-let p1 = new Person("João", 20)
-let p2 = new Person("Maria", 30)
-let p3 = new Person("Mateus", 40)
+let p1 = new Person("João")
+let p2 = new Person("Maria")
+let p3 = new Person("Mateus")
+
+p1.age = 20
+p2.age = 30
+p3.age = 40
+
+console.log(`${p1.name} tem ${p1.age} anos`)
+console.log(`${p2.name} tem ${p2.age} anos`)
+console.log(`${p3.name} tem ${p3.age} anos`)
