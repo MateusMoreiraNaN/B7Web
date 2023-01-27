2131123df91ed69edd64e9f5a03eeb2a710c5bbe
MateusMoreiraNaN <mateusgodoi117@gmail.com>
2023-01-27 14:45:24 -0300

    sobre

--- NodeJS/Módulo1NoçõesbásicasDeNode/src/server.ts
+++ NodeJS/Módulo1NoçõesbásicasDeNode/src/server.ts
@@ -1,9 +1,17 @@
 import express, { Request, Response} from 'express'
 
 const server = express()
-
+//npm run start
 server.get('/',(req: Request, res: Response)=>{
     res.send('Olá Mundo!')
+})
+
+server.get('/contato',(req: Request, res: Response)=>{
+    res.send('Está é a página de contato')
+})
+
+server.get('/sobre',(req: Request, res: Response)=>{
+    res.send('Sobre mim')
 })
 
 server.listen(3000)
