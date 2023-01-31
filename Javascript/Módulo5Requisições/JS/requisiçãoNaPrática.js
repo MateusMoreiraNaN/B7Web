092eb0b237c042685d572a8852a7e3fcd13c5ede
MateusMoreiraNaN <mateusgodoi117@gmail.com>
2023-01-31 14:16:32 -0300

    metodos

--- Javascript/Módulo5Requisições/JS/requisiçãoNaPrática.js
+++ Javascript/Módulo5Requisições/JS/requisiçãoNaPrática.js
@@ -3,8 +3,10 @@
 // Promise = Promessa
 
 function clicou(){
+    // GET, POST, PUT, DELETE
     fetch('https://jsonplaceholder.typicode.com/posts')
         .then((res)=>{
+            console.log(`Status: ${res.status}`);
             return res.json()
         })
         .then((json)=>{
@@ -22,4 +24,27 @@ function somar(x, y){
 }
 */
 
+function inserir(){
+    fetch('https://jsonplaceholder.typicode.com/posts',
+    {
+        method: 'POST',
+        headers:{
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            title: 'Titulo de teste',
+            body: 'Corpo de teste',
+            userId: 2
+        })
+    }
+    )
+    .then((res)=>{
+        return res.json()
+    })
+    .then((json)=>{
+        console.log(json);
+    })
+}
+
 document.querySelector('#botao').addEventListener('click', clicou)
+document.querySelector('#inserir').addEventListener('click', inserir)
