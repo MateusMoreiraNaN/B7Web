e574b19221e88fcf6c8436c2e474de30a3e4f082
MateusMoreiraNaN <mateusgodoi117@gmail.com>
2023-02-01 14:26:46 -0300

    routes

--- NodeJS/Módulo1NoçõesbásicasDeNode/src/server.ts
+++ NodeJS/Módulo1NoçõesbásicasDeNode/src/server.ts
@@ -10,5 +10,9 @@ const server = express()
 server.use('/', mainRoutes)
 server.use('/painel', painelRoutes)
 
+server.use((req: Request, res: Response)=>{
+    res.status(404).send('Pagina não encontrada!')
+});
+
 
 server.listen(3000)
