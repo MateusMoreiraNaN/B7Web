b6ad39def1012c4d8d45bbda7c60cd8bfce88224
MateusMoreiraNaN <mateusgodoi117@gmail.com>
2023-01-31 22:13:44 -0300

    async

--- NodeJS/Módulo1NoçõesbásicasDeNode/src/server.ts
+++ NodeJS/Módulo1NoçõesbásicasDeNode/src/server.ts
@@ -2,20 +2,22 @@ import express, { Request, Response} from 'express'
 
 const server = express()
 //npm run start
+
 server.get('/',(req: Request, res: Response)=>{
     res.send('Olá Mundo!')
 })
 
-server.get('/contato',(req: Request, res: Response)=>{
-    res.send('Está é a página de contato')
+server.get('/noticia/:slug',(req: Request, res: Response)=>{
+    let slug = req.params.slug
+    res.send(`Notícia: ${slug}`)
 })
 
-server.get('/sobre',(req: Request, res: Response)=>{
-    res.send('Sobre mim')
+server.get('/voo/:origem-:destino', (req: Request, res: Response)=>{
+    let { origem, destino } = req.params
+    
+    res.send(`Procurando voos de ${origem.toUpperCase()} até ${destino.toUpperCase()}`)
 })
 
-server.get('/duvidas',(req: Request, res: Response)=>{
-    res.send('Tira as duvidas')
-})
+
 
 server.listen(3000)
