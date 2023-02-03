31750127cd1f4d26c083e9e6fe6f8dc80b2d97a2
MateusMoreiraNaN <mateusgodoi117@gmail.com>
2023-02-03 18:39:54 -0300

    async

--- Javascript/Módulo5Requisições/comments/JS/script.js
+++ Javascript/Módulo5Requisições/comments/JS/script.js
@@ -1,8 +1,44 @@
+/*
 async function clicou(){
     let req = await fetch('https://jsonplaceholder.typicode.com/posts/1/comments')
     
     let json = await req.json()
-    alert(`Titulo do primeiro post: ${json[0].email}`)
+    alert(`Name: ${json[0].name}`)
+    alert(`Email: ${json[0].email}`)
 }
 
 document.querySelector('#botao').addEventListener('click', clicou)
+*/
+
+async function readPosts(){
+    let postArea = document.querySelector('.posts')
+    postArea.innerHTML = 'Carregando'
+
+    let response = await fetch('https://jsonplaceholder.typicode.com/posts/1/comments')
+    let json = await response.json()
+
+    if(json.length > 0){
+        postArea.innerHTML = ''
+        for(let i in json){
+            let postHtml = `<div><h1>${json[i].name}</h1>${json[i].email}</div>`
+            postArea.innerHTML += postHtml
+        }
+
+
+    }else{
+        postArea.innerHTML = 'Nenhum comentario para exibir'
+    }
+}
+
+document.querySelector('#inserButtom').addEventListener('click', ()=>{
+    let name = document.querySelector('#titleField').value
+    let body = document.querySelector('#bodyField').value
+
+    if(name && email){
+       addNewPost(name, body)
+    }else{
+        alert("Preecha todos os campos")
+    }
+})
+
+readPosts()
