7c34b334b072b4ca7e777c4142ea5bf15df29e86
MateusMoreiraNaN <mateusgodoi117@gmail.com>
2023-02-06 15:08:50 -0300

    url

--- NodeJS/Módulo1NoçõesbásicasDeNode/src/routes/index.ts
+++ NodeJS/Módulo1NoçõesbásicasDeNode/src/routes/index.ts
@@ -49,11 +49,31 @@ router.get('/voo/:origem-:destino', (req: Request, res: Response)=>{
 router.get('/nome',(req: Request, res: Response)=>{
 
     let nome: string = req.query.nome as string
+    let idade: string = req.query.idade as string
     res.render('pages/nome',{
-        nome
+        nome,
+        idade
     })
 })
 
+router.get('/idade',(req: Request, res: Response)=>{
+    let mostrarIdade: boolean = false
+    let idade: number = 0
+
+    if(req.query.ano){
+        let anoNascimento: number = parseInt(req.query.ano as string)
+        let anoAtual: number = new Date().getFullYear()
+        idade = anoAtual - anoNascimento
+        mostrarIdade = true
+
+
+    }
+    
+    res.render('pages/idade',{
+        idade,
+        mostrarIdade
+    })
+})
 
 
 export default router
