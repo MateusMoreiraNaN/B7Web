fe72dbe196cef05d411617260c1a88f591dc7a1f
MateusMoreiraNaN <mateusgodoi117@gmail.com>
2023-02-08 12:32:42 -0300

    routes

--- NodeJS/Módulo2ProjetoCanil/src/controllers/pageController.ts
+++ NodeJS/Módulo2ProjetoCanil/src/controllers/pageController.ts
@@ -2,21 +2,41 @@ import { Request, Response } from "express"
 
 export const home = (req: Request, res: Response) =>{
 
-    res.render('pages/page')
+    res.render('pages/page',{
+        banner:{
+            title: 'Todos os animais',
+            background: 'allanimals.jpg'
+        }
+    })
 }
 
 export const dogs = (req: Request, res: Response) =>{
 
-    //res.render('pages/page')
+    res.render('pages/page',{
+        banner:{
+            title: 'Cachorros',
+            background: 'banner_dog.jpg'
+        }
+    })
 }
 
 export const cats = (req: Request, res: Response) =>{
 
-    //res.render('pages/page')
+    res.render('pages/page',{
+        banner:{
+            title: 'Gatos',
+            background: 'banner_cat.jpg'
+        }
+    })
 }
 
 export const fishes = (req: Request, res: Response) =>{
 
-    //res.render('pages/page')
+    res.render('pages/page',{
+        banner:{
+            title: 'Peixes',
+            background: 'banner_fish.jpg'
+        }
+    })
 }
 
