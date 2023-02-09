28f2a6eb7bcd4dd05bd0452fd9d0b71d0c557ede
MateusMoreiraNaN <mateusgodoi117@gmail.com>
2023-02-09 13:33:23 -0300

    pets

--- NodeJS/Módulo2ProjetoCanil/src/models/pet.ts
+++ NodeJS/Módulo2ProjetoCanil/src/models/pet.ts
@@ -1,9 +1,13 @@
+type PetType = 'dog' | 'cat' | 'fish'
+type PetSex = 'Masculino' | 'Feminino'
+
+
 type Pet = {
-    type: 'dog' | 'cat' | 'fish', 
+    type: PetType, 
     image: string,
     name: string,
     color: string,
-    sex: 'Masculino' | 'Feminino'
+    sex: PetSex
 }
 
 const data: Pet[] = [
@@ -131,5 +135,36 @@ const data: Pet[] = [
 export const Pet = {
     getAll: (): Pet[] =>{
         return data
+    },
+    getFromType:(type: PetType): Pet[]=>{
+
+        return data.filter(item => item.type === type)
+
+        /*
+        return data.filter(item =>{
+            if(item.type === type){
+                return true
+            }else{
+                return false
+            }
+        })
+        */
+    },
+    getFromName:(name: string): Pet[] =>{
+
+        return data.filter(item => 
+            item.name.toLowerCase().indexOf(name.toLowerCase()) > -1
+        )
+
+        /*
+        return data.filter(item =>{
+            if(item.name.indexOf(name) > -1){
+                return true
+            }else{
+                return false
+            }
+        })
+        */
     }
 }
+
