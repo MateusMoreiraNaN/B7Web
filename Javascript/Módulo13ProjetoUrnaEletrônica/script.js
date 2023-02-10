6d1f1d5f60ba1954f55a2f1c91f8095950e80221
MateusMoreiraNaN <mateusgodoi117@gmail.com>
2023-02-10 19:12:24 -0300

    Interface

--- Javascript/Módulo13ProjetoUrnaEletrônica/script.js
+++ Javascript/Módulo13ProjetoUrnaEletrônica/script.js
@@ -1,6 +1,65 @@
 let seuVotoPara = document.querySelector('.d-1-1 span')
 let cargo = document.querySelector('.d-1-2 span')
-let descricao = document.querySelector('.4-1-4')
-let aviso = document.querySelector('.4-2')
-let lateral = document.querySelector('.4-1 right')
-let numeros = document.querySelector('.4-1-3')
+let descricao = document.querySelector('.d-1-4')
+let aviso = document.querySelector('.d-2')
+let lateral = document.querySelector('.d-1-right')
+let numeros = document.querySelector('.d-1-3')
+
+let etapaAtual = 0
+let numero  = ""
+
+function comecarEtapa(){
+    let etapa = etapas[etapaAtual]
+
+    let numeroHtml = ''
+
+    for(let i=0;i<etapa.numeros;i++){
+        if(i === 0){
+            numeroHtml += '<div class="numero pisca"></div>'
+        }else{
+            numeroHtml += '<div class="numero"></div>'
+        }
+        
+    }
+
+    seuVotoPara.style.display = 'none'
+    cargo.innerHTML = etapa.titulo
+    descricao.innerHTML = ''
+    aviso.style.display = 'none'
+    lateral.innerHTML = ''
+    numeros.innerHTML = numeroHtml
+}
+
+function atualizar(){
+    alert("Fim do voto")
+}
+
+function clicou(n){
+   let elNumero = document.querySelector('.numero.pisca')
+   if(elNumero !== null){
+        elNumero.innerHTML = n
+        numero = `${numero}${n}`
+        elNumero.classList.remove('pisca')
+        if(elNumero.nextElementSibling !== null){
+            elNumero.nextElementSibling.classList.add('pisca')
+        }else{
+            atualizar()
+        }
+    
+   }
+
+}
+
+function branco(){
+    alert("clicou branco")
+}
+
+function corrige(){
+    alert("clicou corrige")
+}
+
+function confirma(){
+    alert("confirma")
+}
+
+comecarEtapa()
