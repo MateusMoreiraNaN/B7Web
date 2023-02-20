6a6219661f777b084883d987badd96717f87aa5e
MateusMoreiraNaN <mateusgodoi117@gmail.com>
2023-02-20 17:22:21 -0300

    Models

--- NodeJS/Módulo3BancoDeDadosSQL/src/controllers/homeController.ts
+++ NodeJS/Módulo3BancoDeDadosSQL/src/controllers/homeController.ts
@@ -6,6 +6,7 @@ import { User } from '../models/User'
 
 
 export const home = async (req: Request, res: Response)=>{
+    /*
     let users = await User.findAll({
         //attributes: ['name', 'age']
 
@@ -14,44 +15,44 @@ export const home = async (req: Request, res: Response)=>{
         where:{
             age:{
                 [Op.gte]: 10
-                /*
+                
                 [Op.gt]: 40, // > 40
                 [Op.gte]: 40, // >= 40
                 [Op.lt]: 40, // < 40
                 [Op.lte]: 40 // <= 40
-                */
+                
 
-                /*
+                
                 [Op.gte]: 40,
                 [Op.lte]: 70
-                */
+                
 
                 //[Op.between]: [40, 80]
                 //[Op.notBetween]: [40, 80]
                 //[Op.in]: [30, 55]
                 //[Op.notIn]: [30, 55]
             },
-            /*
+            
             age:{
                 [Op.or]: [30, 55]
             }
-            */
-            /*
+            
+            
             [Op.or]:[
                 {age: 55},
                 {age: 20},
                 {name: 'Mateus'}
             ]
-            */
+            
             name:{
                 [Op.like]: '%a%'
             }
         },
-        /*
+        
         where:{
             age:[55,90]
         }
-        */
+        
 
         order:[
             ['name', 'ASC'],
@@ -65,11 +66,24 @@ export const home = async (req: Request, res: Response)=>{
 
         
     })
-
+    
+    */
     //console.log("USUARIOS: ", JSON.stringify(users));
 
+    // build + save
+    /*
+    const user = User.build({
+        name: 'fulaninho',
+        age: 25
+    })
+    await user.save()
+    */
 
-    
+    // create
+    const user = await User.create({
+        name: 'bot',
+        age: 100
+    })
 
 
 
@@ -109,6 +123,6 @@ export const home = async (req: Request, res: Response)=>{
         products: list,
         expensives: expensiveList,
         frasesDoDia: [],
-        users
+        //users
     });
 };
