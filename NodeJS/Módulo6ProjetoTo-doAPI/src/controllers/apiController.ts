ab9f78a05fcf52c2494fb70d316740f11db33c54
MateusMoreiraNaN <mateusgodoi117@gmail.com>
2023-03-27 19:14:37 -0300

    nav

--- NodeJS/Módulo6ProjetoTo-doAPI/src/controllers/apiController.ts
+++ NodeJS/Módulo6ProjetoTo-doAPI/src/controllers/apiController.ts
@@ -1,3 +1,20 @@
 import { Request, Response } from "express";
 import { join } from "path";
-import { Sequelize } from 'sequelize';
+import { Sequelize } from 'sequelize'
+import { Todo } from "../models/to-do"
+
+export const all = async () =>{
+
+}
+
+export const add = async () =>{
+    
+}
+
+export const update = async () =>{
+    
+}
+
+export const remove = async () =>{
+    
+}
--- NodeJS/Módulo6ProjetoTo-doAPI/src/routes/api.ts
+++ NodeJS/Módulo6ProjetoTo-doAPI/src/routes/api.ts
@@ -4,6 +4,9 @@ import * as apiController from '../controllers/apiController'
 
 const router = Router();
 
-
+router.get('/todos', apiController.all)
+router.post('/todo', apiController.add)
+router.put('/todo/:id', apiController.update)
+router.delete('/todo/:id', apiController.remove)
 
 export default router;
