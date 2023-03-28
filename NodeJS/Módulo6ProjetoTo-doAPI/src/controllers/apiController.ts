7c6fd69d7c9a8bd39e60ec426cbc79999c7780e8
MateusMoreiraNaN <mateusgodoi117@gmail.com>
2023-03-28 18:01:59 -0300

    nav

--- NodeJS/Módulo6ProjetoTo-doAPI/src/controllers/apiController.ts
+++ NodeJS/Módulo6ProjetoTo-doAPI/src/controllers/apiController.ts
@@ -3,18 +3,18 @@ import { join } from "path";
 import { Sequelize } from 'sequelize'
 import { Todo } from "../models/to-do"
 
-export const all = async () =>{
-
+export const all = async (req: Request, res: Response) =>{
+    const list = await Todo.findAll()
 }
 
-export const add = async () =>{
+export const add = async (req: Request, res: Response) =>{
     
 }
 
-export const update = async () =>{
+export const update = async (req: Request, res: Response) =>{
     
 }
 
-export const remove = async () =>{
+export const remove = async (req: Request, res: Response) =>{
     
 }
