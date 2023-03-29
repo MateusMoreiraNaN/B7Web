246baf83e37d2c2fd99638f4da2a3b79a3746b7b
MateusMoreiraNaN <mateusgodoi117@gmail.com>
2023-03-29 18:49:27 -0300

    create

--- NodeJS/Módulo6ProjetoTo-doAPI/src/controllers/apiController.ts
+++ NodeJS/Módulo6ProjetoTo-doAPI/src/controllers/apiController.ts
@@ -5,10 +5,22 @@ import { Todo } from "../models/to-do"
 
 export const all = async (req: Request, res: Response) =>{
     const list = await Todo.findAll()
+
+    res.json({ list })
 }
 
 export const add = async (req: Request, res: Response) =>{
-    
+    if(req.body.title){
+        let newTodo = await Todo.create({
+            title: req.body.title,
+            done: req.body.done ? true : false
+        })
+
+        res.status(201).json({item: newTodo})
+    }else{
+        res.json({ error: 'Erro.' })
+    }
+
 }
 
 export const update = async (req: Request, res: Response) =>{
