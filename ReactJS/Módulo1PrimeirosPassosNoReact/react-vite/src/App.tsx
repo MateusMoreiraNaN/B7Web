94a59735a033b95bf78a81852d268da0307a39d3
MateusMoreiraNaN <mateusgodoi117@gmail.com>
2023-05-08 18:53:57 -0300

    JSX

--- ReactJS/Módulo1PrimeirosPassosNoReact/react-vite/src/App.tsx
+++ ReactJS/Módulo1PrimeirosPassosNoReact/react-vite/src/App.tsx
@@ -1,9 +1,15 @@
 import React from "react"
 
 const App = () =>{
+
+    let name: string = 'Mateus'
+    let n1: number = 10
+    let n2: number = 3
     return(
         <div>
-            Opa, Tudo bem?
+            Opa {name}, Tudo bem?
+
+            {n1 * n2}
         
             (<a href="https://www.youtube.com/">Clique aqui</a>)
         </div>
