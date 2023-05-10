c73829be9b6b3c08a2f1aa9abe0258b4bf5fe051
MateusMoreiraNaN <mateusgodoi117@gmail.com>
2023-05-09 23:52:14 -0300

    header

--- ReactJS/Módulo1PrimeirosPassosNoReact/react-vite/src/App.tsx
+++ ReactJS/Módulo1PrimeirosPassosNoReact/react-vite/src/App.tsx
@@ -4,7 +4,17 @@ const Header = () =>{
     return (
         <header>
             <h1>Titulo da página</h1>
+            <br />
         </header>
+    )
+}
+
+const F = () =>{
+    return(
+        <footer>
+            <br />
+            <h2>Final</h2>
+        </footer>
     )
 }
 
@@ -14,12 +24,16 @@ const App = () =>{
     let n1: number = 10
     let n2: number = 3
     return(
+        
         <div>
+            <Header/>
             Opa {name}, Tudo bem?
 
             {n1 * n2}
         
             (<a href="https://www.youtube.com/">Clique aqui</a>)
+
+            <F/>
         </div>
     )
 }
