50cfd1ad9a3a92034a278295e6425bda11291b57
MateusMoreiraNaN <mateusgodoi117@gmail.com>
2023-05-23 14:06:36 -0300

    eventos

--- ReactJS/Módulo2States&Eventos/react-vite/src/App.tsx
+++ ReactJS/Módulo2States&Eventos/react-vite/src/App.tsx
@@ -1,15 +1,30 @@
 import {  } from 'react'
 import './App.css'
+import { useState } from 'react'
 
 const App = ()=>{
+  const [numero, setNumero] = useState(0)
+  const [name, setName] = useState('Bonieky')
 
   const clickHand = ()=>{
-    alert("O botão foi clicado!")
+    //alert("O botão foi clicado!")
+    //numero += 10
+
+    //setNumero()
+
+  }
+
+  const clickMudar = ()=>{
+      setName('Mateus')
   }
 
     return(
       <div>
+          <p>O numero é:{numero}</p>
           <button onClick={clickHand}>Clique aqui</button>
+
+          <p>Meu nome é:{name}</p>
+          <button onClick={clickMudar}>Mudar nome</button>
       </div>
     )
 }
