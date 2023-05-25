1b98f3860876f9aad449c93314588fbb59ee7d73
MateusMoreiraNaN <mateusgodoi117@gmail.com>
2023-05-25 13:57:53 -0300

    save

--- ReactJS/Módulo2States&Eventos/componentes/src/App.tsx
+++ ReactJS/Módulo2States&Eventos/componentes/src/App.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import { Botao } from './components/Botao'
+import { Pessoa } from './components/Pessoa'
 
 import './App.css'
 
@@ -11,9 +12,25 @@ const App = ()=>{
     alert("Frase do App: "+txt)
   }
 
+  let list = [
+    {name: 'Bonieky', age: '90'},
+    {name: 'João', age: '21'},
+    {name: 'Mateus', age: '38'},
+    {name: 'Pedro', age: '90'},,
+    {name: 'Paulo', age: '90'},
+  ]
+
     return(
       <div>
         <Botao text={textoDoBotao} clickFn={botaoEvent}/>
+
+        <h2>Lista de presença</h2>
+        <ul>
+         {list.map((i, index)=>(
+          //<li key={index}>{i?.name} - {i?.age} anos</li>
+          <Pessoa key={index} data={i} />
+         ))}
+        </ul>
       </div>
     )
 }
