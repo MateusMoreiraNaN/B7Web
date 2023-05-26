dbd0b358487f6c78e202467a31c2bf2db38247eb
MateusMoreiraNaN <mateusgodoi117@gmail.com>
2023-05-26 13:37:43 -0300

    save

--- ReactJS/Módulo3Listas&Estilização/estilização/src/App.tsx
+++ ReactJS/Módulo3Listas&Estilização/estilização/src/App.tsx
@@ -2,9 +2,28 @@ import { useState } from 'react'
 import './App.css'
 
 const App = () =>{
+  const [clicked, setClick] = useState(false)
+  const [padding, setPadding] = useState(50)
+
+  const handleClick = ()=>{
+    if(clicked == false){
+      setClick(true)
+    }else{
+      setClick(false)
+    }
+
+    if(padding == 50){
+      setPadding(100)
+    }else{
+      setPadding(50)
+    }
+  }
+
+  
+
   return(
     <div>
-      <button style={{backgroundColor: '#FF0000'}}>Clique aqui</button>
+      <button onClick={handleClick}style={{backgroundColor: clicked ? '#273746' : '#E67E22 ', padding: padding }}>Clique aqui</button>
     </div>
   )
 }
