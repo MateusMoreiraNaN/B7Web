23b4ec144eb30f1b4585ec8c4480e66516399e9e
MateusMoreiraNaN <mateusgodoi117@gmail.com>
2023-05-28 15:09:06 -0300

    save

--- ReactJS/Módulo3Listas&Estilização/TailwindCSS/src/App.tsx
+++ ReactJS/Módulo3Listas&Estilização/TailwindCSS/src/App.tsx
@@ -3,8 +3,8 @@ import './App.css'
 
 const App = ()=>{
     return(
-      <div className='bg-black text-orange-500 p-8'>Texto qualquer
-        <div className="text-5xl">
+      <div className='bg-black text-orange-500 p-8 flex flex-col hover:text-6xl'><p className='hover:bg-teal-500'>Texto qualquer</p>
+        <div className=" hover:bg-red-500 text-5xl  mt-8">
           <p className="text-sky-400/100">The quick brown fox...</p>
           <p className="text-sky-400/75">The quick brown fox...</p>
           <p className="text-sky-400/50">The quick brown fox...</p>
