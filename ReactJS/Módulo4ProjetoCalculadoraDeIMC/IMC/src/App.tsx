0a1ff825355a1a0fa0b031f5aa7ca36d25f23ba4
MateusMoreiraNaN <mateusgodoi117@gmail.com>
2023-05-28 19:35:54 -0300

    save

--- ReactJS/Módulo4ProjetoCalculadoraDeIMC/IMC/src/App.tsx
+++ ReactJS/Módulo4ProjetoCalculadoraDeIMC/IMC/src/App.tsx
@@ -4,6 +4,9 @@ import poweredImage from './assets/imc/powered.png'
 import styles from './App.module.css'
 
 const App = ()=>{
+  const [heightField, setHeightField] = useState<number>(0)
+  const [weightField, setWeightField] = useState<number>(0)
+
   return(
     <div className={styles.main}>
       <header>
@@ -13,7 +16,27 @@ const App = ()=>{
       </header>
       <div className={styles.container}>
         <div className={styles.leftSide}>
-          ...
+          <h1>Calcule seu IMC.</h1>
+          <p>IMC é a sigla para Indice de Massa Corpóreea. parâmetro adotado pela Organização mundial de Saúde para calcular o peso Ideal de cada pessoa.
+          </p>
+
+          <input 
+            type="number"
+            placeholder='Digite a sua altura. Ex 1.5 (em metros)'
+            value={heightField > 0 ? heightField : ''}
+            onChange={e => setHeightField(parseFloat(e.target.value))}
+            
+          />
+
+<input 
+            type="number"
+            placeholder='Digite o seu peso 75.3 (em kg)'
+            value={weightField > 0 ? weightField : ''}
+            onChange={e => setWeightField(parseFloat(e.target.value))}
+            
+          />
+
+          <button>Calcular</button>
         </div>
 
         <div className={styles.rightSide}>
