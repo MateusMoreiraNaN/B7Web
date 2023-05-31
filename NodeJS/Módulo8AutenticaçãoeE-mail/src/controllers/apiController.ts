8bdcd0195a29265c80fde88f2f56423e17baff3d
MateusMoreiraNaN <mateusgodoi117@gmail.com>
2023-05-31 15:45:04 -0300

    add

--- NodeJS/Módulo8AutenticaçãoeE-mail/src/controllers/apiController.ts
+++ NodeJS/Módulo8AutenticaçãoeE-mail/src/controllers/apiController.ts
@@ -1,22 +1,28 @@
 import { Request, Response } from "express";
 import { Auth } from "../model/authModel";
 
-export const ping = (req: Request, res: Response)=>{
-    res.json({pong: true})
-}
+
 
 export const register = async(req:Request, res: Response)=>{
-    let { email, password } = req.body
+    if(req.body.email && req.body.password){
+        let { email, password } = req.body
 
-    let hasUser = await Auth.findOne({where:{email}})
+        let hasUser = await Auth.findOne({where:{email}})
 
-    if(!hasUser){
-        let newUser = await Auth.create({email, password})
-
-        res.status(201).json({id: newUser.id})
-    }else{
-        res.json({error:'E-mail já existe'})
+        if(hasUser){
+            let newUser = await Auth.create({email, password})
+    
+    
+            res.json({id: newUser.id})
+        }else{
+            res.json({error:'E-mail já existe'})
+        }
     }
+
+    res.json({error: 'E-mail e/ou senha não enviados'})
+
+
+    
 }
 
 export const login = async(req: Request, res: Response)=>{
--- NodeJS/Módulo8AutenticaçãoeE-mail/src/model/authModel.ts
+++ NodeJS/Módulo8AutenticaçãoeE-mail/src/model/authModel.ts
@@ -1,5 +1,5 @@
 import { Model, DataTypes } from "sequelize"
-import { sequelize } from "../mysql/mysql"
+import { sequelize } from "../database/mysql"
 
 export interface authInstance extends Model{
     id: number
