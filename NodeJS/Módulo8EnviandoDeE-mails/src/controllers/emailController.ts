5f3ca82c9bf41a51a8b2a9473c0af05a6ac6f1ce
MateusMoreiraNaN <mateusgodoi117@gmail.com>
2023-06-02 13:24:53 -0300

    git

--- NodeJS/Módulo8EnviandoDeE-mails/src/controllers/emailController.ts
+++ NodeJS/Módulo8EnviandoDeE-mails/src/controllers/emailController.ts
@@ -3,18 +3,33 @@ import { Auth } from "../model/authModel";
 import nodemailer from 'nodemailer'
 
 
-export const contato = ()=>{
+export const contato = async (req: Request, res: Response)=>{
     // Passo 1: Configurar o transporter
-    let transport = nodemailer.createTransport({
+    var transport = nodemailer.createTransport({
         host: "sandbox.smtp.mailtrap.io",
         port: 2525,
         auth: {
-          user: "13f0f1e790dff2",
-          pass: "********fdb7"
+          user: "fb25343a265ef0",
+          pass: "fcf7376d4c47d5"
         }
       });
 
 
     // Passo 2: Configurar a mensagem
+    let message = {
+        from: 'não-responda@b7web.com',
+        to: 'fulanoG@hotmail.com',
+        replyTo: req.body.from,
+        subject: req.body.subject,
+        html: req.body.email,
+        text: req.body.email
+    }
     // Passo 3: enviar a mensagem
+
+    let info = await transport.sendMail(message)
+
+    console.log("INFO",info);
+
+    res.json({success: true})
+    
 }
