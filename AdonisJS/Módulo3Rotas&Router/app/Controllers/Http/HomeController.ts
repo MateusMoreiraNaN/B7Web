87f525033f6e4637c6c3dac8c019d85bfcc3a2e8
MateusMoreiraNaN <mateusgodoi117@gmail.com>
2023-06-11 19:01:46 -0300

    git

--- AdonisJS/Módulo3Rotas&Router/app/Controllers/Http/HomeController.ts
+++ AdonisJS/Módulo3Rotas&Router/app/Controllers/Http/HomeController.ts
@@ -1,15 +1,9 @@
 // import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 
-
-
 export default class HomeController {
-
-
     async index({view}){
         return view.render('welcome')
         //return 'Hello World'
         
     }
 }
-
-
--- AdonisJS/Módulo3Rotas&Router/start/routes.ts
+++ AdonisJS/Módulo3Rotas&Router/start/routes.ts
@@ -20,6 +20,5 @@
 
 import Route from '@ioc:Adonis/Core/Route'
 
-Route.get('/', async ({ view }) => {
-  return view.render('welcome')
-})
+
+Route.get('/', 'HomeController.index')
