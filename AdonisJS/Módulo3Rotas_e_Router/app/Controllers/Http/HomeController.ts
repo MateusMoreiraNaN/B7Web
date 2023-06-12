0846a86b7354f1233f1f333b0d2a2535eb6e6888
MateusMoreiraNaN <mateusgodoi117@gmail.com>
2023-06-12 19:13:32 -0300

    git

--- AdonisJS/Módulo3Rotas_e_Router/app/Controllers/Http/HomeController.ts
+++ AdonisJS/Módulo3Rotas_e_Router/app/Controllers/Http/HomeController.ts
@@ -5,13 +5,38 @@ export default class HomeController {
     
     async index({view}){
         let dados = {
+            
             usuario:{
                 usuario: 'Alessandro Kobs',
                 idade: 123,
                 apelido: 'Kobs',
                 tecnologias: ['html', 'css', 'js'],
                 //tecnologiasTexto: ['html', 'css', 'js'].join(',')
-            }
+            },
+            
+
+            usuarios:[
+                {
+                    nome: 'Pedro',
+                    tecnologias:['java', 'php', 'python']
+                },
+                {
+                    nome: 'Mateus',
+                    tecnologias:['node', 'css', 'JS']
+                },
+                {
+                    nome: 'Tiago',
+                    tecnologias:['jogo digital', 'ios', 'node']
+                },
+                {
+                    nome: 'Fulano',
+                    tecnologias:['adonis', 'java', 'c']
+                },
+                {
+                    nome: 'Julia',
+                    tecnologias:['C#', 'js', 'c++']
+                }
+            ]
         }
         return view.render('painel/home', dados)
         //return 'Hello World'
